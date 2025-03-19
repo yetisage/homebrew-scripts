effb53441766102ca9393dd1a1cdeb5eaf2c896f
Tobias Lahrmann Hansen <tlh@netic.dk>
2025-03-19 12:48:26 +0100

    update sha

--- Formula/zellij-run.rb
+++ Formula/zellij-run.rb
@@ -4,7 +4,7 @@ class ZellijRun < Formula
   url "https://github.com/yetisage/zellij-run/releases/download/v0.1.0/zellij-run-0.1.0.tar.gz"
   version "0.1.0"
   revision 4
-  sha256 "996af5068ae656bc4340d2d2bae2305c6077207bb3c8f7467faebbc0f500f67b"
+  sha256 "5b02681276ea25736c5eb0d38f29b4b726afe276856168fb89ec1175b45543e6"
   depends_on "zellij"
 
   def install
