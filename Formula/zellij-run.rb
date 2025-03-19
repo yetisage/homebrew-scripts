77a7beead2a81f13e3af46d2f4a07777588887d5
Tobias Lahrmann Hansen <tlh@netic.dk>
2025-03-19 12:35:01 +0100

    Remove templating that does not work

--- Formula/zellij-run.rb
+++ Formula/zellij-run.rb
@@ -1,9 +1,9 @@
 class ZellijRun < Formula
   desc "Run commands in Zellij panes or fall back to regular bash"
   homepage "https://github.com/yetisage/zellij-run"
-  url "https://github.com/yetisage/zellij-run/releases/download/v#{version}/zellij-run-#{version}.tar.gz"
+  url "https://github.com/yetisage/zellij-run/releases/download/v0.1.0/zellij-run-0.1.0.tar.gz"
   version "0.1.0"
-  revision 2
+  revision 3
   sha256 "fa0a620f0cc7ef9d27a3dd581109e23ec151efc69344937ee3e99f33acf82b86"
   depends_on "zellij"
 
