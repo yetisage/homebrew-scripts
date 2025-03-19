a0682e9561613a0f20b9df1d8e47ee71ff3f707f
Tobias Lahrmann Hansen <tlh@netic.dk>
2025-03-19 13:21:23 +0100

    update revision and url

--- Formula/zellij-run.rb
+++ Formula/zellij-run.rb
@@ -1,9 +1,9 @@
 class ZellijRun < Formula
   desc "Run commands in Zellij panes or fall back to regular bash"
   homepage "https://github.com/yetisage/zellij-run"
-  url "https://github.com/yetisage/zellij-run/releases/download/v0.1.0/zellij-run-0.1.0.tar.gz"
+  url "https://github.com/yetisage/zellij-run/archive/refs/tags/v0.1.0.tar.gz"
   version "0.1.0"
-  revision 5
+  revision 6
   sha256 "5b02681276ea25736c5eb0d38f29b4b726afe276856168fb89ec1175b45543e6"
   depends_on "zellij"
 
